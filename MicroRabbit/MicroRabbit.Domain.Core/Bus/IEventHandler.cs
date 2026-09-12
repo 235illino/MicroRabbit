ddf061aa1ccc1ea90b58afd82a4f898df56a34e5
ILLia <235illino@gmail.com>
2026-09-12 15:24:07 +0200

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+    public interface IEventHandler
+    {
+    }
+}
