52a4ebecdb96470818de9e196ee10e843e2dfb92
ILLia <235illino@gmail.com>
2026-09-13 14:18:11 +0200

    add using MicroRabbit.Infra.Bus

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,8 @@
 ﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MicroRabbit.Infra.IoC;
+namespace MicroRabbit.Infra.IoC
 
 public class DependencyContainer
 {
