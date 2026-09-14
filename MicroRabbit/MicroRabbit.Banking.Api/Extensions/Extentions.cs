5081792d61b201365eaaa523c6c4a16981398e53
ILLia <235illino@gmail.com>
2026-09-14 22:31:34 +0200

    Partialy Completed Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Extensions/Extentions.cs
@@ -0,0 +1,11 @@
+﻿using MicroRabbit.Infra.IoC;
+
+namespace MicroRabbit.Banking.Api.Extensions;
+
+public static class Extensions
+{
+    public static void RegisterServices(this IHostApplicationBuilder builder)
+    {
+        DependencyContainer.RegisterServices(builder.Services);
+    }
+}
--- MicroRabbit/MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Program.cs
@@ -1,17 +1,29 @@
+using MicroRabbit.Banking.Api.Extensions;
+using MicroRabbit.Banking.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BankingDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly));
+
+builder.RegisterServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
